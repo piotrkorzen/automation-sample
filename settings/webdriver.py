7e1eb6fba03c7fe3d9f6427586a384fba3d8e931
piotrkorzen <piotrkorzen90@gmail.com>
2019-04-05 18:59:40 +0200

    New tests

--- settings/webdriver.py
+++ settings/webdriver.py
@@ -12,7 +12,7 @@ class Driver:
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        self.instance = webdriver.Chrome(options=chrome_options, executable_path=r'chromedriver.exe')
+        self.instance = webdriver.Chrome(options=chrome_options, executable_path='chromedriver.exe')
 
     def navigate(self, url):
         if isinstance(url, str):
