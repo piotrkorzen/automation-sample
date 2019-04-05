f8cedea1c7a201280902ec73ee8f02c6b77a20cb
piotrkorzen <46975031+piotrkorzen@users.noreply.github.com>
2019-04-05 18:49:03 +0200

    Update webdriver.py

--- settings/webdriver.py
+++ settings/webdriver.py
@@ -9,7 +9,7 @@ class Driver:
         chrome_options = webdriver.ChromeOptions()
         prefs = {"profile.default_content_setting_values.notifications": 1}
         chrome_options.add_experimental_option("prefs", prefs)
-        self.instance = webdriver.Chrome(options=chrome_options, executable_path=r'/usr/share/applications/google-chrome.desktop')
+        self.instance = webdriver.Chrome(options=chrome_options, executable_path=r'./chromedriver.exe')
 
     def navigate(self, url):
         if isinstance(url, str):
