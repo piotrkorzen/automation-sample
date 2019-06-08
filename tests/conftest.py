19ef83976f7dcc07e8c16185ee78a6b04fe822cb
piotrkorzen <piotr.korzen@encja.com>
2019-06-08 18:07:33 +0200

    crossbrowser

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,17 +5,12 @@ def pytest_addoption(parser):
     parser.addoption('--u', action='store', default='', help='url environment address')
     parser.addoption('--lg', action='store', default='', help='user environment login')
     parser.addoption('--pwd', action='store', default='', help='user environment password')
-    parser.addoption('--ack', action='store', default='', help='api customer key')
-    parser.addoption('--alog', action='store', default='', help='api authorization login')
-    parser.addoption('--ap', action='store', default='', help='api authorization password')
-    parser.addoption('--loops', action='store', default='', help='amount of loops in tests or tools')
+    parser.addoption('--b', action='store', default='CH', help='browser')
+
 
 
 def pytest_configure(config):
     os.environ['u'] = config.getoption('u')
     os.environ['lg'] = config.getoption('lg')
     os.environ['pwd'] = config.getoption('pwd')
-    os.environ['ack'] = config.getoption('ack')
-    os.environ['alog'] = config.getoption('alog')
-    os.environ['ap'] = config.getoption('ap')
-    os.environ['loops'] = config.getoption('loops')
+    os.environ['b'] = config.getoption('b')
--- tests/test_images_carousel.py
+++ tests/test_images_carousel.py
@@ -4,17 +4,16 @@ from page_objects.home_page.categories import *
 from page_objects.product_page.productImages import *
 import time
 
-@pytest.mark.usefixtures("scope")
 class TestImagesCarousel():
 
-    # @pytest.fixture()
-    # def test_setup(self):
-    #     # self.driver = Driver()
-    #
-    #     yield
-    #     self.driver.instance.quit()
+    @pytest.fixture()
+    def test_setup(self):
+        self.driver = Driver()
 
-    def test_images_carousel(self):
+        yield
+        self.driver.instance.quit()
+
+    def test_images_carousel(self, test_setup):
         self.prod = WomenCategoriesProducts(self.driver)
         self.image = ProductImages(self.driver)
         self.driver.navigate("http://automationpractice.com/index.php?id_category=3&controller=category")
