9fb90ec003d8c2438d3ecf9b34e7ee31f258c429
piotrkorzen <piotr.korzen@encja.com>
2019-07-27 18:05:05 +0200

    headless on

--- settings/webdriver.py
+++ settings/webdriver.py
@@ -7,7 +7,7 @@ class Driver:
 
         if os.getenv('BROWSER') == "CH":
             chrome_options = webdriver.ChromeOptions()
-            # chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--headless')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('window-size=1600,1200')
