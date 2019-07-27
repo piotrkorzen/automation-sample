4fa4cfb9b18a882d51c194b130f17ff3d80162df
piotrkorzen <piotr.korzen@encja.com>
2019-07-27 18:44:03 +0200

    screenshot

--- tests/test_auth_page_dropdowns.py
+++ tests/test_auth_page_dropdowns.py
@@ -24,9 +24,8 @@ class TestAuth():
         self.bp.click(AuthLocators.SUBMIT_CREATE)
         yield
         now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')
-        allure.attach(self.driver.instance.get_screenshot_as_png('./tests/screenshots/{}.png'.format(now)),
-                      name="Screenshot",
-                      attachment_type=AttachmentType.PNG)
+        allure.attach(self.driver.instance.get_screenshot_as_png(name="{}".format(now),
+                      attachment_type=AttachmentType.PNG))
 
         self.driver.instance.quit()
 
