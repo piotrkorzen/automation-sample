1c620fe877c6ad068e5420cc14cab1154a4f3167
piotrkorzen <piotr.korzen@encja.com>
2019-07-28 14:51:12 +0200

    add decorators to base_page functions

--- page_objects/base_page.py
+++ page_objects/base_page.py
@@ -25,10 +25,11 @@ class BasePage(object):
         else:
             return By.ID, value
 
+    @allure.step("Getting the " + str({1}) + " has been successful")
     def get(self, url):
         return self.driver.navigate(url)
 
-    @allure.step("The element" + str({0}) + "has been clicked")
+    @allure.step("The element " + str({1}) + " has been clicked")
     def click(self, value, wait=15):
         """function for click on single element"""
         try:
@@ -38,6 +39,7 @@ class BasePage(object):
             return WebDriverWait(self.driver.instance, wait).until(EC.visibility_of_element_located(
                 (self.locator(value)))).click()
 
+    @allure.step("The element " + str({1}) + " with position " + str({2}) + " has been clicked")
     def click_element(self, value, element_number=0):
         """function for click on element from list of elements where we can choose which element is interesting for us
            e.g. we can choose particular product from product scope or iterate through all products"""
@@ -48,6 +50,7 @@ class BasePage(object):
             self.elements_list.append(element)
         self.elements_list[element_number].click()
 
+    @allure.step("The element " + str({1}) + " has been set")
     def set(self, value, send_value):
         """function for setting input values, checkboxes, radio buttons and dropdown lists"""
         inputs = ["input", "textarea"]
@@ -83,23 +86,28 @@ class BasePage(object):
                 except NoSuchElementException:
                     self.option.select_by_index(send_value)
 
+    @allure.step("The element " + str({1}) + " has been presented")
     def validate_element_present(self, value):
         condition = EC.visibility_of_element_located(self.locator(value))
         element = WebDriverWait(self.driver.instance, timeout=20).until(condition)
         assert element.is_displayed()
 
+    @allure.step("The element " + str({1}) + " has been NOT presented")
     def validate_element_not_present(self, value):
         condition = EC.invisibility_of_element_located(self.locator(value))
         element = WebDriverWait(self.driver.instance, timeout=10).until(condition)
         assert element == True
 
+    @allure.step("Current url is equal to expected" + str({1}))
     def validate_url(self, url):
         WebDriverWait(self.driver.instance, 10).until(
             lambda driver: self.driver.instance.current_url == url)
 
+    @allure.step("Switched to window" + str({1}) + " has been successful")
     def switch_to_window(self, window):
         self.driver.instance.switch_to.window(self.driver.instance.window_handles[window])
 
+    @allure.step("Script " + str({1}) + " has been executed")
     def execute_script(self, *script):
         self.driver.instance.execute_script(script)
 
@@ -107,6 +115,7 @@ class BasePage(object):
         WebDriverWait(self.driver.instance, timeout=10).until(EC.visibility_of_element_located(
             (self.locator(value))))
 
+    @allure.step("Mouse is on " + str({1}) + " and element" + str({2}))
     def hover(self, value, element=0):
         self.all_elements = []
         products = WebDriverWait(self.driver.instance, timeout=10).until(
@@ -116,6 +125,7 @@ class BasePage(object):
         hover = ActionChains(self.driver.instance).move_to_element(self.all_elements[element])
         hover.perform()
 
+    @allure.step("The elements from " + str({1}) + " has been taken")
     def get_items_from_dropdown(self, value):
         """function returning items from dropdowns lists"""
         self.items = []
@@ -131,6 +141,7 @@ class BasePage(object):
         logging.info("The number of items on the list is: {}".format(self.items_counter))
         return self.items
 
+    @allure.step("The item " + str({2}) + " has been selected")
     def validate_item_is_selected(self, value, send_value):
         """function for validate element from dropdown list is selected correctly"""
         self.set(value, send_value)
@@ -140,6 +151,7 @@ class BasePage(object):
             logging.error("Item {} has not been selected".format(value))
             raise AssertionError("Items have not been selected")
 
+    @allure.step("The slider " + str({1}) + " is on a position")
     def slider(self, value, *offset):
         slider = WebDriverWait(self.driver.instance, 10).until(EC.visibility_of_element_located(
             (self.locator(value))))
