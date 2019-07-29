1e32c7ac577bc755588e42bc0f9573720489678b
Piotr Korzeń <piotr.korzen@encja.com>
2019-07-29 21:53:57 +0200

    webdriver paths

--- settings/webdriver.py
+++ settings/webdriver.py
@@ -12,12 +12,12 @@ class Driver:
             chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('window-size=1600,1200')
             self.instance = webdriver.Chrome(options=chrome_options,
-                                             executable_path='./chromedriver.exe')
+                                             executable_path='./automationpractice.com/chromedriver.exe')
         elif os.getenv('BROWSER') == "FF":
             ff_options = webdriver.FirefoxOptions()
             # ff_options.headless = True
             self.instance = webdriver.Firefox(options=ff_options,
-                                              executable_path='/home/piotrko/PycharmProjects/automationpractice.com/geckodriver.exe')
+                                              executable_path='./automationpractice.com/geckodriver.exe')
         elif os.getenv('BROWSER') == "OP":
             self.instance = webdriver.Opera(
                 executable_path='./automationpractice.com/operadriver.exe')
