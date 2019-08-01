2da397478254c96d6191609f308388d56c1f79c5
Piotr Korzeń <piotr.korzen@encja.com>
2019-08-01 21:27:21 +0200

    PyVirtualDisplay separate instances

--- settings/webdriver.py
+++ settings/webdriver.py
@@ -5,13 +5,15 @@ from pyvirtualdisplay import Display
 
 class Driver:
     VISIBLE = 1  # 0 - do not run GUI mode, 1 - run GUI mode
+    ORIG = os.environ["DISPLAY"]
 
     def __init__(self):
         """PyVirtualDisplay library for run tests without GUI mode e.g. on Jenkins.
         On machine must be installed xvfb module or Xephyr - sudo apt-get install xvfb xserver-xephyr"""
         self.display = Display(visible=self.VISIBLE, size=(2560, 2560))
         self.display.start()
-
+        # reset of DISPLAY instance - it is neccesery for parallel testing
+        os.environ["DISPLAY"] = self.ORIG
         if os.getenv('BROWSER') == "CH":
             self.instance = webdriver.Chrome(
                 executable_path='./automationpractice.com/chromedriver.exe')
