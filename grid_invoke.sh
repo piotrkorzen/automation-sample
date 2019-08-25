28103c3133ccd1a41c38e737e94e6a29bc75ad6e
Piotr Korzeń <piotr.korzen@encja.com>
2019-08-25 11:26:30 +0200

    enable screenshots

--- grid_invoke.sh
+++ grid_invoke.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Running Selenium Server..."
-java -Dwebdriver.chrome.driver=./chromedriver.exe -jar ./selenium_grid/selenium-server-standalone-3.141.0.jar -role hub -hubConfig ./selenium_grid/conf.json & xvfb-run --server-args="-screen 0, 2560x2560x24" java -Dwebdriver.chrome.driver=./chromedriver.exe -jar ./selenium_grid/selenium-server-standalone-3.141.0.jar -role node -nodeConfig ./selenium_grid/node.json &
+java -Dwebdriver.chrome.driver=./chromedriver.exe -jar ./selenium_grid/selenium-server-standalone-3.141.0.jar -role hub -hubConfig ./selenium_grid/conf.json & xvfb-run --server-args="-screen 0, 2560x2560x24" java -Dwebdriver.chrome.driver=./chromedriver.exe -jar ./selenium_grid/selenium-server-standalone-3.141.0.jar -role node -nodeConfig ./selenium_grid/node.json
