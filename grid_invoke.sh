a5abc78cc7c03056e3ad3439088e54fb0ca4dcb2
Piotr Korzeń <piotrkorzen90@encja.com>
2019-10-26 18:46:20 +0200

    update chromedriver to v78

--- grid_invoke.sh
+++ grid_invoke.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+VISIBLE=1
+
 echo "Running Selenium Server..."
-java -Dwebdriver.chrome.driver=./chromedriver.exe -jar ./selenium_grid/selenium-server-standalone-3.141.0.jar -role hub -hubConfig ./selenium_grid/conf.json & xvfb-run --server-args="-screen 0, 2560x2560x24" java -Dwebdriver.chrome.driver=./chromedriver.exe -jar ./selenium_grid/selenium-server-standalone-3.141.0.jar -role node -nodeConfig ./selenium_grid/node.json
+if [ $VISIBLE == 0 ]; then
+    java -Dwebdriver.chrome.driver=./chromedriver.exe.exe -jar ./selenium_grid/selenium-server-standalone-3.141.0.jar -role hub -hubConfig ./selenium_grid/conf.json & xvfb-run --server-args="-screen 0, 2560x2560x24" java -Dwebdriver.chrome.driver=./chromedriver.exe.exe -jar ./selenium_grid/selenium-server-standalone-3.141.0.jar -role node -nodeConfig ./selenium_grid/node.json
+else
+      java -Dwebdriver.chrome.driver=./chromedriver.exe.exe -jar ./selenium_grid/selenium-server-standalone-3.141.0.jar -role hub -hubConfig ./selenium_grid/conf.json & java -Dwebdriver.chrome.driver=./chromedriver.exe.exe -jar ./selenium_grid/selenium-server-standalone-3.141.0.jar -role node -nodeConfig ./selenium_grid/node.json
+fi
